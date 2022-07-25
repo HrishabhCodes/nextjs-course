e3e01ceeadbfe196c5bd175291f4a20bf14fe94f
Hrishabh Jain <hrishabh507@gmail.com>
2022-07-25 17:40:00 +0530

    V11 - Routing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 const Home = () => {
-  return <h1>Home Page</h1>;
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Placing your order...");
+    router.replace("/product");
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <br />
+      <Link href="/product">
+        <a>Products</a>
+      </Link>
+      <br />
+      <button onClick={handleClick}>Place Order</button>
+    </div>
+  );
 };
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,31 @@
-const Product = () => {
+import Link from "next/link";
+
+const Product = ({ productId = 100 }) => {
   return (
     <div>
-      <h2>Product-1</h2>
-      <h2>Product-2</h2>
-      <h2>Product-3</h2>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/product/1">
+        <a>
+          <h2>Product-1</h2>
+        </a>
+      </Link>
+      <Link href="/product/2">
+        <a>
+          <h2>Product-2</h2>
+        </a>
+      </Link>
+      <Link href="/product/3" replace>
+        <a>
+          <h2>Product-3</h2>
+        </a>
+      </Link>
+      <Link href={`/product/${productId}`}>
+        <a>
+          <h2>Product-{productId}</h2>
+        </a>
+      </Link>
     </div>
   );
 };
